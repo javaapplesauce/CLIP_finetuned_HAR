7f8846397bff38ddbb2b30a8aa3f55f7698cdbfd
javaapplesauce <42728229+javaapplesauce@users.noreply.github.com>
2025-07-26 14:57:28 -0700

    train.py building

--- load_data.py
+++ load_data.py
@@ -1,56 +1,21 @@
 import os
+from typing import Tuple
 
-# Torch imports
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.optim.lr_scheduler import (
-    CosineAnnealingLR,
-    CosineAnnealingWarmRestarts,
-    MultiStepLR,
-    ReduceLROnPlateau,
-    SequentialLR,
-    LinearLR
-)
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
-from transformers import AutoModelForImageClassification
-import torch.distributed as dist
-from torchvision.transforms import InterpolationMode
-from PIL import Image
-
-# Multiprocess imports
-import torch.multiprocessing as mp
+from torch.utils.data import DataLoader, Dataset
 from torch.utils.data.distributed import DistributedSampler
-from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.distributed import init_process_group, destroy_process_group
+from torchvision import transforms
+from torchvision.transforms import InterpolationMode
 
-# Dataset imports
-from datasets import load_dataset, DownloadConfig
-from collections import Counter
-import hydra
-from omegaconf import DictConfig, OmegaConf
-from rich import pretty
-
-# Metrics imports
-from sklearn.metrics import precision_score, recall_score, f1_score, average_precision_score
-import numpy as np
-import random
-import wandb
-import time
-import psutil
-from datetime import datetime, timedelta
-import sys
-import math
-import pandas
+from datasets import load_dataset
+from omegaconf import DictConfig
 
 class HFDataset(Dataset):
     """
     Wraps a Hugging Face Dataset for PyTorch DataLoader.
     Applies image transforms and returns (tensor, label).
     """
-    def __init__(self, hf_dataset, transform):
+    def __init__(self, hf_dataset, transform: transforms.Compose):
         self.dataset = hf_dataset
         self.transform = transform
 
@@ -64,61 +29,29 @@ class HFDataset(Dataset):
         label = item["labels"]  # integer label 0-14
         return img_tensor, label
     
-    @staticmethod
-    def transform():
+def prepare_dataloader(dataset: Dataset, 
+                        batch_size: int,
+                        num_workers: int, 
+                        shuffle: bool = False) -> DataLoader:
+    sampler = DistributedSampler(dataset, shuffle=shuffle)
+        return DataLoader(
+            dataset,
+            batch_size=batch_size,
+            sampler=sampler,
+            num_workers=num_workers,
+            pin_memory=True,
+        )
         
-        return transforms.Compose([
-
-            transforms.RandomResizedCrop(
-                224,
-                scale=(0.9, 1.0),               # default in PyTorch
-                interpolation=InterpolationMode.BICUBIC
-            ),
-            transforms.Lambda(lambda img: img.convert("RGB")),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=CLIP_MEAN, std=CLIP_STD),
-        ])
-        
-        # return transforms.Compose([
-        #     transforms.RandomResizedCrop(
-        #         224,
-        #         scale=(0.08, 1.0),               # default in PyTorch
-        #         ratio=(0.75, 1.3333333),
-        #         interpolation=InterpolationMode.BICUBIC
-        #     ),
-        #     transforms.RandomHorizontalFlip(p=0.5),
-        #     transforms.ToTensor(),
-        #     transforms.Normalize(CLIP_MEAN, CLIP_STD),
-        # ])
-
-        # base = [
-        #     transforms.Resize(256),
-        #     transforms.RandomResizedCrop(224, scale=(0.8,1.0)),
-        #     transforms.RandomHorizontalFlip(p=0.5),
-        #     transforms.ColorJitter(brightness=.2, contrast=.2, saturation=.2, hue=.1),
-        #     transforms.ToTensor(),
-        #     transforms.Normalize((0.5,)*3, (0.5,)*3),
-        # ]
-        # return transforms.Compose(base)
-
-def prepare_dataloader(dataset: Dataset, batch_size: int):
-    return DataLoader(
-        dataset,
-        batch_size=batch_size,
-        pin_memory=True,
-        sampler=DistributedSampler(dataset)
-    )
-
-def load_data():
+def load_data(cfg: DictConfig) -> Tuple[DataLoader, DataLoader, DataLoader]:
 
     # loads dataset
     ds = load_dataset("Bingsu/Human_Action_Recognition")  # load your dataset
 
     ds_train = dataset["train"]
     # Create train/val split since official test labels are all zero
-    split = ds_train.train_test_split(test_size=0.2, seed=42, shuffle=True)
+    split = ds_train.train_test_split(test_size=cfg.data.test_size, seed=cfg.data.seed, shuffle=cfg.data.shuffle)
     
-    training = split["train"].train_test_split(test_size=0.2, seed=42, shuffle=True)
+    training = split["train"].train_test_split(test_size=cfg.data.test_size, seed=cfg.data.seed, shuffle=cfg.data.shuffle)
     train_ds = training["train"]
     val_ds = training["test"]
     
@@ -131,8 +64,8 @@ def load_data():
     test_dataset   = HFDataset(test_ds,   HFDataset.transform())
 
     # loads test, validation, and training dataset
-    train_data = prepare_dataloader(train_dataset, batch_size)
-    val_data = prepare_dataloader(val_dataset, batch_size)
-    test_data = prepare_dataloader(test_dataset, batch_size)
+    train_data = prepare_dataloader(train_dataset, batch_size=cfg.data.batch_size, num_workers=cfg.data.num_workers)
+    val_data = prepare_dataloader(val_dataset, batch_size=cfg.data.batch_size, num_workers=cfg.data.num_workers)
+    test_data = prepare_dataloader(test_dataset, batch_size=cfg.data.batch_size, num_workers=cfg.data.num_workers)
 
     return train_data, val_data, test_data
--- train.py
+++ train.py
@@ -5,14 +5,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.optim.lr_scheduler import (
-    CosineAnnealingLR,
-    CosineAnnealingWarmRestarts,
-    MultiStepLR,
-    ReduceLROnPlateau,
-    SequentialLR,
-    LinearLR
-)
+import torch.optim.lr_scheduler as lr_scheduler 
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from transformers import AutoModelForImageClassification
@@ -65,26 +58,145 @@ def wandb_init(cfg: DictConfig):
 
 ### build the model (default ViT-32)
 def build_model(cfg: DictConfig):
-    model = {
-        'CLIP-ViT-B': build_model_clip,
-        'CLIP-ViT-L': build_model_clip,
-    }[cfg.model.backbone](cfg)
-    return model
+    return AutoModelForImageClassification.from_pretrained(
+        "openai/clip-vit-base-patch32",
+        num_labels=15)
 
 
 ### build the criterion
+class SignLoss(nn.Module):
+    def forward(self, class_logits, targets):
+        assert class_logits.size() == targets.size(), "dimension mismatch"
 
+        batch_size, n_classes = class_logits.size()
+        zeros = torch.zeros(batch_size).view(batch_size, -1).to(targets.device)
+        
+        class_logits[targets == 1] *= -1
+        class_logits = torch.cat((zeros, class_logits), dim=1)
+
+        loss = torch.logsumexp(class_logits, 1)
+
+        return loss
+
+def build_criterion(cfg, train_loader):
+    if cfg.loss.name == 'WBCE':
+        pos_weights = train_loader.dataset.get_pos_weights()
+        criterion = nn.BCEWithLogitsLoss(pos_weight=torch.tensor(pos_weights).cuda())
+    elif cfg.loss.name == 'Sign' or cfg.loss.name == 'SignLoss':
+        criterion = SignLoss()
+    else:
+        raise ValueError
+    
+    return criterion
 
 ### build the optimizer
-
+def build_optimizer(cfg, model):
+    optimizer = torch.optim.AdamW(model.parameters(), lr=cfg.optim.base_lr, weight_decay=cfg.optim.weight_decay)
 
 ### build the scheduler
+def build_scheduler(cfg, optimizer):
+    if cfg.scheduler.name == 'CosineAnnealingWarmRestarts':
+            scheduler = lr_scheduler.CosineAnnealingWarmRestarts(
+                optimizer,
+                T_0=cfg.scheduler.T_0,
+                T_mult=cfg.scheduler.T_mult
+            )
+    else:
+        raise NotImplemented()
 
+    return scheduler
 
 ### data loading --> load_data.py
 
 
 ### training loop
+class Trainer:
+
+    def __init__(self, cfg, local_rank) -> None:
+        self.cfg = cfg
+        self.global_rank = dist.get_rank()
+        self.world_size = dist.get_world_size()
+        self.local_rank = local_rank
+        self.local_gpus = torch.cuda.device_count()
+
+        self.global_master = self.global_rank == 0
+        self.local_master = self.local_rank == 0
+
+    def train(self):
+        if self.global_master:
+            wandb_init(self.cfg)
+        
+        model = build_model(self.cfg).to(self.local_rank)
+        model = DDP(model, device_ids=[self.local_rank], find_unused_parameters=False)
+
+
+
+        train_data, val_data, test_data = load_data.load_data(self.cfg)
+        self.train_data = train_data  # Store for loop
+        self.val_data = val_data
+        self.test_data = test_data
+        self.cfg.data.n_iters = len(train_data)
+
+        criterion = build_criterion(self.cfg, train_loader)
+        optimizer = build_optimizer(self.cfg, model)
+        scheduler = build_scheduler(self.cfg, optimizer)
+        ema = torch.optim.swa_utils.AveragedModel(model) if self.cfg.get("use_ema", False) else None
+
+        for epoch in range(self.cfg.epochs):
+            val_data.set_epoch(epoch)
+            metric = Metric()
+            model.train()
+
+            if self.global_master:
+                wandb.log({'lr': optimizer.param_groups[0]['lr']}, step=epoch)
+
+            for it, (source, targets) in enumerate(self.train_data):
+                start_time = time.time()
+                
+                source = source.to(self.local_rank)
+                targets = targets.to(self.local_rank)
+                
+                optimizer.zero_grad()
+
+                output = model(source)
+                logits  = output.logits
+                targets_onehot = torch.nn.F.one_hot(
+                    targets, num_classes=15).float()
+                
+                loss_values = criterion(logits, targets_onehot.to(logits.device))
+                loss = loss_values.mean()
+                loss.backward()            
+                optimizer.step()
+
+                if ema:
+                    ema.update_parameters(model)
+
+                if self.cfg.scheduler.name == 'CosineAnnealingWarmRestarts':
+                    scheduler.step(epoch + it / self.cfg.data.n_iters)
+                else:
+                    scheduler.step()  
+              
+                metric.add_val(loss.item())
+
+                batch_time = time.time() - start_time
+
+                if self.global_master and it % self.cfg.train.print_freq == 0:
+                    self.log(
+                        f"Epoch: {epoch}/{self.cfg.epochs}, "
+                        f"Iter: {it}/{len(train_data)}, "
+                        f"Loss: {loss.item():.4f} ({metric.mean:.4f}), "
+                        f"Time: {batch_time:.2f}s"
+                    )
+
+                    wandb.log({
+                        "train/loss": loss.item(),
+                        "train/loss_avg": metric.mean,
+                        "train/batch_time": batch_time,
+                        "train/epoch": epoch,
+                    }, step=epoch * self.cfg.data.n_iters + it)
+        
+
+
 
 
 ### validation
@@ -116,7 +228,5 @@ def main(cfg: DictConfig):
     torch.cuda.set_device(local_rank)
     device = torch.device("cuda", cfg.local_rank)
     
-    train_data, val_data, test_data = load_data.load_data()
-
     trainer = Trainer(cfg, cfg.local_rank)
     trainer.train()
